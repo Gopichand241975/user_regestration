44ee07068f7fb22548ef98df202b144a155cdcab
Gopichand241975 <gopichand241975@gmail.com>
2025-11-13 15:08:37 +0530

    Add files via upload

---
+++ user_demo.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS user_demo;
+USE user_demo;
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
